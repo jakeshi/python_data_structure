17f46539f473c7dbd5e71139e3f42b711c589cd6
Jake <jakehku@gmail.com>
2017-12-19 10:13:24 -0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,1 +1,24 @@
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+        
 class Linked_List:
+
+    def __init__(self):
+        self.head = None
+        
+    def isEmpty(self):
+        return self.head == None
